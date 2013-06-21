be3e19b29a0d6093ea499a56d83ada068e679c01
Reshad Farid <reshadfar@gmail.com>
2013-06-21 22:32:41 +0200

    finished cv

---
+++ index.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+<html>
+  <head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# object: http://ogp.me/ns/object#">
+  <meta property="fb:app_id" content="449514805128575" /> 
+  <meta property="og:type"   content="object" /> 
+  <meta property="og:url"    content="http://www.reshadfarid.nl" /> 
+  <meta property="og:title"  content="Reshad Farid" /> 
+  <meta property="og:image"  content="img/foto.jpg" /> 
+  <meta charset="UTF-8">
+  <title>Reshad Farid</title>
+  <meta name="viewport" content="width=device-width; initial-scale=1.0">
+  <meta name="author" content="Reshad Farid">
+  <meta name="description" content="Curriculum vitae of Reshad Farid">
+  <meta name="keywords" content="Reshad Farid,Developer">
+  <link href='http://fonts.googleapis.com/css?family=Scada:700,400' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="css/style.css?version=1" type="text/css" media="screen">
+  <link rel="stylesheet" href="css/print.css" type="text/css" media="print">
+
+  <!--[if lt IE 9]>
+	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+ </head>
+ <body>
+ 	<article class="container">
+ 		<section class="personal">
+ 				<h2 class="title">Personal</h2>
+ 				<figure id="photo">
+ 					<img src="img/foto.jpg">
+ 					<figcaption>
+ 						<ul>
+ 							<li><a href="javascript:if(window.print)window.print()">Print Resume</a></li>
+ 							<li><a href="http://www.linkedin.com/pub/reshad-farid/4a/3b7/83b" target="_blank">LinkedIn</a></li>
+ 							<li><a href="http://dribbble.com/Reshadd" target="_blank">Dribbble</a></li>
+              <li><script type="text/javascript" src="//cdn.dev.skype.com/uri/skype-uri.js"></script>
+<div id="SkypeButton_Chat_reshad.farid_1">
+  <script type="text/javascript">
+    Skype.ui({
+      "name": "chat",
+      "element": "SkypeButton_Chat_reshad.farid_1",
+      "participants": ["reshad.farid"],
+      "imageColor": "blue",
+      "imageSize": 32
+    });
+  </script>
+</div></li>
+ 						</ul>
+ 					</figcaption>
+ 				</figure>
+ 			<table>
+        <tr>
+          <td>Name:</td>
+          <td>Reshad Farid</td>
+        </tr>
+ 				<tr>
+ 					<td>Date of Birth:</td>
+ 					<td>December 16th, 1992</td>
+ 				</tr>
+ 				<tr>
+ 					<td>Address:</td>
+ 					<td>Haarlem, The Netherlands</td>
+ 				</tr>
+ 				<tr>
+ 					<td>Email:</td>
+ 					<td>reshadfar@gmail.com</td>
+ 				</tr>
+ 				<tr>
+ 					<td>Phone:</td>
+ 					<td>(+31)613266263</td>
+ 				</tr>
+ 				<tr>
+ 					<td>Website:</td>
+ 					<td><a href="www.reshadfarid.nl">http://www.reshadfarid.nl</a></td>
+ 				</tr>
+ 			</table>
+ 		</section>
+ 		<section class="skills">
+ 				<h2 class="title">Skills</h2>
+ 				<table>
+ 					<tr>
+ 						<td>PHP:</td>
+ 						<td>
+ 							<div class="classification"><div class="cover"></div><div class="progress" style="width: 80%;"></div><div class="textprogress">Goed</div></div>
+ 						</td>
+ 					</tr>
+ 					<tr>
+ 						<td>Java:</td>
+ 						<td>
+ 							<div class="classification"><div class="cover"></div><div class="progress" style="width: 75%;"></div><div class="textprogress">Redelijk goed</div></div>
+ 						</td>
+ 					</tr>
+ 					<tr>
+ 						<td>HTML / CSS:</td>
+ 						<td>
+ 							<div class="classification"><div class="cover"></div><div class="progress" style="width: 100%;"></div><div class="textprogress">Uitstekend</div></div>
+ 						</td>
+ 					</tr>
+ 					<tr>
+ 						<td>Javascript / jQuery:</td>
+ 						<td>
+ 							<div class="classification"><div class="cover"></div><div class="progress" style="width: 75%;"></div><div class="textprogress">Goed</div></div>
+ 						</td>
+ 					</tr>
+ 					<tr>
+ 						<td>GIT:</td>
+ 						<td>
+ 							<div class="classification"><div class="cover"></div><div class="progress" style="width: 90%;"></div><div class="textprogress">Goed</div></div>
+ 						</td>
+ 					</tr>
+          <tr>
+            <td>Ruby(on Rails):</td>
+            <td>
+              <div class="classification"><div class="cover"></div><div class="progress" style="width: 10%;"></div><div class="textprogress">Niet zo goed</div></div>
+            </td>
+          </tr>
+          <tr>
+            <td>Photoshop:</td>
+            <td>
+              <div class="classification"><div class="cover"></div><div class="progress" style="width: 80%;"></div><div class="textprogress">goed</div></div>
+            </td>
+          </tr>
+ 				</table>
+
+ 		</section>
+ 		<section class="experience">
+ 				<h2 class="title">Experience</h2>
+ 				<table>
+ 					<tr>
+ 						<td><h3>Het Betere BoerenBed</h3>
+ 							<br>
+ 							<small><h4>Traineeship</h4> <br>
+ 								Created a new website for a international company that provides sleeping accomodations at farms in 5 different countries<br>
+ 							</small>
+ 							<span>website: <a href="http://www.featherdown.com" target="_blank">www.boerenbed.nl</a></span>
+ 						</td>
+ 						<td class="date">2012</td>
+ 					</tr>
+          <tr>
+            <td><h3>Bella taxi Haarlem</h3>
+              <br>
+              <small><h4>Website development</h4> <br>
+                Created a new website for a taxi company that works in Haarlem/Amsterdam/Schiphol-Airport<br>
+              </small>
+              <span>website: <a href="http://www.bellataxihaarlem.nl" target="_blank">www.bellataxihaarlem.nl</a></span>
+            </td>
+            <td class="date">2012</td>
+          </tr>
+ 				</table>
+
+ 		</section>
+ 		<section class="education">
+ 				<h2 class="title">Education</h2>
+ 				<table>
+ 					<tr>
+ 						<td><h3>Hogeschool Leiden</h3><br>
+ 							<small>Information and Communication Technologies</small>
+ 						</td>
+ 						<td class="date">2012/13 - PRESENT TIME</td>
+ 					</tr>
+ 					<tr>
+ 						<td><h3>Nova College Haarlem</h3><br>
+ 							<small>Application Development</small>
+ 						</td>
+ 						<td class="date">2008/09 - 2012-13</td>
+ 					</tr>
+ 				</table>
+
+ 		</section>
+ 		<section class="interests">
+ 				<h2 class="title">Interests</h2>
+ 				<p>
+          In my spare time I love to solve problems that are related to programming,
+          to improve applications that I have created in the past, or create new applications to learn new or better techniques.
+        </p>
+        <p>
+          Please feel free to contact me whenever necessary!
+        </p>
+ 		</section>
+ 	</article>
+  
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js" type="text/javascript"></script>
+  <script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-30524183-1']);
+  _gaq.push(['_setDomainName', 'reshadfarid.nl']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+ </body>
+</html>
